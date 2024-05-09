ff8616906e52a3703eb50a800230b2eaa0349fe5
Kalai <skl.rsn@gmail.com>
2024-05-09 23:23:10 +0300

    CORS fixes

---
+++ kube-two/backend/go.mod
@@ -0,0 +1,20 @@
+module github.com/sklrsn/kube-works/kube-two/backend
+
+go 1.22.3
+
+require github.com/lib/pq v1.10.9
+
+require github.com/jmoiron/sqlx v1.4.0
+
+require (
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/go-redis/cache/v9 v9.0.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/redis/go-redis/v9 v9.0.0-rc.4 // indirect
+	github.com/vmihailenco/go-tinylfu v0.2.2 // indirect
+	github.com/vmihailenco/msgpack/v5 v5.3.4 // indirect
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+)
---
+++ kube-two/backend/main.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/go-redis/cache/v9"
+	"github.com/google/uuid"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+	"github.com/redis/go-redis/v9"
+)
+
+func init() {
+}
+
+var (
+	db         *sqlx.DB
+	redisCache *cache.Cache
+	err        error
+)
+
+func CreateTable() (sql.Result, error) {
+	q := `
+		CREATE TABLE IF NOT EXISTS messages (
+			id VARCHAR,
+			message VARCHAR
+		)
+	`
+	return db.Exec(q)
+}
+
+func ConnectDb() (*sqlx.DB, error) {
+	return sqlx.Connect("postgres",
+		fmt.Sprintf("host=%v user=postgres password=%v dbname=postgres sslmode=disable port=5432",
+			os.Getenv("DB_HOST"), os.Getenv("DB_PASSWORD")))
+}
+
+func ConnectRedis() *cache.Cache {
+	ring := redis.NewRing(&redis.RingOptions{
+
+		Addrs: map[string]string{
+			os.Getenv("REDIS_HOST"): os.Getenv("REDIS_PORT"),
+		},
+		Password: "",
+	})
+	return cache.New(&cache.Options{
+		Redis:      ring,
+		LocalCache: cache.NewTinyLFU(1000, time.Hour),
+	})
+}
+
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		next.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+	sync.OnceFunc(func() {
+		db, err = ConnectDb()
+		if err != nil {
+			panic(err)
+		}
+		if _, err := CreateTable(); err != nil {
+			panic(err)
+		}
+		redisCache = ConnectRedis()
+	})()
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/message", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		log.Printf("received a request from %v", r.RemoteAddr)
+		var m interface{}
+		if err := json.NewDecoder(r.Body).Decode(&m); err != nil {
+			log.Printf("%v", err)
+			http.Error(w, "failed to decode message", http.StatusBadRequest)
+			return
+		}
+		bs, err := json.Marshal(m)
+		if err != nil {
+			log.Printf("%v", err)
+			http.Error(w, "failed to marshal message", http.StatusBadRequest)
+			return
+		}
+		_data := make(map[string]interface{})
+		_data["data"] = base64.StdEncoding.EncodeToString(bs)
+		_data["created"] = time.Now().UTC()
+
+		data, err := json.Marshal(_data)
+		if err != nil {
+			log.Printf("%v", err)
+			http.Error(w, "failed to marshal message", http.StatusBadRequest)
+			return
+		}
+
+		type Data struct {
+			ID      string `db:"id"`
+			Message string `db:"message"`
+		}
+
+		id := uuid.NewString()
+		msg := Data{
+			ID:      id,
+			Message: string(data),
+		}
+		msgInBytes, err := json.Marshal(msg)
+		if err != nil {
+			log.Printf("%v", err)
+			http.Error(w, "failed to marshal message", http.StatusBadRequest)
+			return
+		}
+
+		//Cache
+		redisCache.Set(&cache.Item{
+			Ctx:   context.Background(),
+			Key:   id,
+			Value: string(msgInBytes),
+		})
+
+		//DB
+		if _, err := db.NamedExecContext(
+			context.Background(),
+			"INSERT INTO messages(id, message) VALUES(:id,:message)", msg); err != nil {
+			log.Printf("%v", err)
+			http.Error(w, "failed to write message to db", http.StatusBadRequest)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+	})
+
+	handler := corsMiddleware(mux)
+
+	log.Println("backend is ready to serve traffic")
+	log.Fatalf("%v", http.ListenAndServe(":9090", handler))
+}
